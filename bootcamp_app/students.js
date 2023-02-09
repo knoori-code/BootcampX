c84538ce5b17db54ad1ff878d285fb01387a641e
knoori-code <knoori3@gmail.com>
2023-02-09 01:04:12 -0500

    Use parameterized queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const [ cohortName, numberOfResults ] = process.argv.slice(2);
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name = '${cohortName}12'
-LIMIT ${numberOfResults};
-`)
-.then(res => {
-  console.log(res.rows)
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
+WHERE cohorts.name = $1
+LIMIT $2;
+`;
+
+const [cohortName, numberOfResults] = process.argv.slice(2);
+const values = [cohortName, numberOfResults];
+
+pool.query(queryString, values).then((res) => {
+  console.log(res.rows);
+  res.rows.forEach((user) => {
+    console.log(
+      `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+    );
+  });
 });
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const [ cohortName ] = process.argv.slice(2);
+const [cohortName] = process.argv.slice(2);
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName}';
-`)
-.then(res => {
-  console.log(res.rows)
-  res.rows.forEach(row => {
+WHERE cohorts.name = $1;
+`;
+
+const value = [cohortName];
+
+pool.query(queryString, value).then((res) => {
+  console.log(res.rows);
+  res.rows.forEach((row) => {
     console.log(`${row.name}: ${row.teacher} `);
-  })
+  });
 });
